4b524d976a22cff109c581c517bcdfc40f62fb5f
Lauren Scott <wisconsinpoet@gmail.com>
2014-02-21 11:16:57 -0600

    working on card controllers

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,8 +1,7 @@
-post '/add_cards' do
-	@deck = Deck.create(params[:deck])
-	erb :'deck/add_cards'
-end
 
 post '/card_added' do
-	Card.create(params[:card])
+
+	card = Card.create(params[:card])
+	redirect to 
 end
+
