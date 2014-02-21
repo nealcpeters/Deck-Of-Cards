f24c4d42cd082ceffe52241973f3452f4a73d887
dandydanny <dandydanny@gmail.com>
2014-02-21 11:03:42 -0600

    added basic schema fields for all migrations

--- db/migrate/20140221105421_create_decks.rb
+++ db/migrate/20140221105421_create_decks.rb
@@ -1,4 +1,10 @@
 class CreateDecks < ActiveRecord::Migration
   def change
+    create_table :decks do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20140221105434_create_answers.rb
+++ db/migrate/20140221105434_create_answers.rb
@@ -1,4 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.integer :user_id
+      t.integer :card_id
+      t.integer :round_id
+      t.integer :result
+    end
   end
 end
