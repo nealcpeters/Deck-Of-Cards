3fcecd8b24817b8e313b6c738b419b681a884d6d
nealcpeters <nealcpeters@gmail.com>
2014-02-21 14:18:20 -0600

    add user views

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,9 +8,11 @@ get '/user/sign_in' do
   erb :"user_views/sign_in"
 end
 
-get '/user/:id' do
 
-  erb :"user_views/show"
+get '/user/sign_out' do
+  p session
+  session[:user_id] = nil
+  redirect to('/')
 end
 
 get '/user/edit/:id' do
@@ -18,25 +20,25 @@ get '/user/edit/:id' do
   erb :"user_views/edit"
 end
 
+get '/user/:id' do
+
+  erb :"user_views/show"
+end
+
 post '/user/new' do
 
   user = User.new({
-  	email: params[:email],
-  	password: params[:password],
-  	password_confirmation: params[:password_confirmation]
-  	})
-  if user.save?
+    email: params[:email],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation]
+    })
+  if user.save
     session[:user_id] = user.id
-    redirect to('/user/new')
+    redirect to("/user/#{user.id}")
   else
-  	@errors = user.errors.messages
-  	erb :"/user/new"
+    @errors = user.errors.messages
+    erb :"user_views/new"
   end
-end
-
-post '/user/sign_out' do
-  session[:user_id] = nil
-  redirect to('/')
 end
 
 post '/user/sign_in' do
@@ -44,9 +46,9 @@ post '/user/sign_in' do
 
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
-    redirect_to("/user/#{user.id}")
+    redirect to("/user/#{user.id}")
   else
     @errors = user.errors.messages
-    erb :"/user_views/sign_in"
+    erb :"user_views/sign_in"
   end
 end
